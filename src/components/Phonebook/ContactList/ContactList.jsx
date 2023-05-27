fc1db91c686c7332dd8bc9b2e92cc2de58acb9d1
ESM <111918644+HoneATCS@users.noreply.github.com>
2023-05-27 20:20:37 +0300

    correct

---
+++ src/components/Phonebook/ContactList/ContactList.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import PropTypes from 'prop-types';
+import { selectItem } from 'redux/contacts/contactsSelectors';
+import { deleteContact } from 'redux/contacts/requestAPI';
+import { selectFilter } from 'redux/contacts/contactsSelectors';
+import ContactListSCSS from './ContactList.module.scss';
+
+const ContactList = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectItem);
+  const filter = useSelector(selectFilter);
+  const delContacts = event => {
+    dispatch(deleteContact(event.target.name));
+  };
+  const contactFilter = () => {
+    if (filter === '') {
+      return false;
+    }
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter)
+    );
+  };
+  const filterOne = contactFilter();
+  const list = filterOne ? filterOne : contacts;
+  return (
+    <ul className={ContactListSCSS.list}>
+      {list.map(({ id, name, number }) => (
+        <li key={id} className={ContactListSCSS.item}>
+          <p className={ContactListSCSS.par}>
+            {name}: {number}
+          </p>
+          <button
+            className={ContactListSCSS.button}
+            type="button"
+            name={id}
+            onClick={delContacts}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+// ContactList.propTypes = {
+//   contacts: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.string.isRequired,
+//       name: PropTypes.string.isRequired,
+//       number: PropTypes.string.isRequired,
+//     })
+//   ),
+//   deleteContacts: PropTypes.func.isRequired,
+// };
+
+export default ContactList;
